e44f8261c3d60f7133109a07432fc71e111b7820
Anagha1Ranganath <45656311+Anagha1Ranganath@users.noreply.github.com>
2018-12-20 12:50:36 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[])
+    {
+        Scanner input=new Scanner(System.in);
+
+        System.out.println("Enter a string");
+        String str=input.nextLine();
+
+        System.out.println("Enter a value");
+        int n=input.nextInt();
+
+        int count=str.length();
+
+        while(n>=1)
+        String strstore[i]=str[j];
+
+        System.out.print(str);
+        for(i=0;i<n;i++)
+        {
+            System.out.println(strstore);
+        }
+    }
+    )
+}
